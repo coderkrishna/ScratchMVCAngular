65d01ca0bb0d619dac9c3d60cd6dda41426ed6c2
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-05-06 19:16:52 -0400

    Further tests being done

--- ScratchMVCAngular.Tests/CalculatorComponentTests.cs
+++ ScratchMVCAngular.Tests/CalculatorComponentTests.cs
@@ -38,5 +38,23 @@ namespace ScratchMVCAngular.Tests
                 Console.WriteLine("The VerifyMedian test has passed!");
             }
         }
+
+        [TestMethod]
+        public void VerifyMultiMode()
+        {
+            var calc = new Operations();
+            int[] inputInts = new int[] { 1, 2, 1, 3, 4, 2, 8, 6 };
+
+            int[] expected = new int[] { 1, 2 }; 
+            int[] actual = calc.CalculateMultiMode(inputInts);
+
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+                Console.WriteLine("Mode: {0}, ExpectedMode: {1}, Result: {2}", actual[i], expected[i], "YES");
+            }
+
+            Console.WriteLine("The VerifyMultiMode test passed");
+        }
     }
 }
