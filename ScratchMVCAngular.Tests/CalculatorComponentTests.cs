0929b22bf1e33d21e6525ce822b00214eb6341d7
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-05-06 14:06:49 -0400

    Latest code

--- ScratchMVCAngular.Tests/CalculatorComponentTests.cs
+++ ScratchMVCAngular.Tests/CalculatorComponentTests.cs
@@ -12,7 +12,15 @@ namespace ScratchMVCAngular.Tests
         {
             var calc = new Operations();
             int[] inputInts = new int[] { 1, 3, 4, 2 }; 
-            var expectedMean = calc.CalculateAverage(inputInts); 
+            var expectedMean = calc.CalculateAverage(inputInts);
+
+            double testMean = 2.5;
+            Assert.AreEqual(expectedMean, testMean);
+
+            if (expectedMean == testMean)
+            {
+                Console.WriteLine("The VerifyMean test has passed!");
+            }
         }
     }
 }
